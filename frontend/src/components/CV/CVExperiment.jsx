719879b08b2c8f76ed31aa43adf9820ee174e0dd
Nami <naminaziri1@gmail.com>
2024-07-01 20:16:31 +0300

    Update

--- frontend/src/components/CV/CVExperiment.jsx
+++ frontend/src/components/CV/CVExperiment.jsx
@@ -5,14 +5,14 @@ import "./CVExperiment.css"
 function CVExperiment() {
   return (
     <Grid className="rowStyle" columnSpacing={{ xs: 2, md: 5 }} container display="flex" height='100vh' justifyContent="center" alignItems="center">
-       <Grid xs={12}  justifyContent="center" alignItems="top">
-    <h1>Education</h1>
-    </Grid>
-    <Grid xs={12}  justifyContent="center" alignItems="top">
-      <CVElement></CVElement>
+      <Grid xs={12}  justifyContent="center" alignItems="top">
+        <h1>Education</h1>
       </Grid>
       <Grid xs={12}  justifyContent="center" alignItems="top">
-      <CVElement></CVElement>
+        <CVElement></CVElement>
+      </Grid>
+      <Grid xs={12}  justifyContent="center" alignItems="top">
+        <CVElement></CVElement>
       </Grid>
     </Grid>
   )
